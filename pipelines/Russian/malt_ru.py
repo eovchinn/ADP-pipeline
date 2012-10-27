a0ead82869e6f84f591cfe20becac7b2389c5b76
Vladimir Zaytsev <zaytsev@usc.edu>
2012-10-26 20:57:23 -0700

    added sample outputs for Russian pipeline

--- pipelines/Russian/malt_ru.py
+++ pipelines/Russian/malt_ru.py
@@ -10,7 +10,7 @@ import sys
 import argparse
 
 
-conll_splitter = re.compile("\s+")
+line_splitter = re.compile("\s+")
 postag_map = {
     "V": ("vb", 4),
     "A": ("adj", 2),
@@ -20,27 +20,32 @@ postag_map = {
 }
 
 
-def conll_mapper(conl_str, sent_i=0):
-    cid, form, lemma, cpostag, postag, \
-        feats, head, deprel, phead, pdeprel =\
-        conll_splitter.split(conl_str)
-    return None
-
-
 def main():
-    parser = argparse.ArgumentParser(description="MALT output processing pipeline for Russian.")
-    parser.add_argument("--input", help="The input CoNLL file to be processed. Default is stdin.", default=None)
-    parser.add_argument("--output", help="Output file. Default is stdout.", default=None)
+    parser = argparse.ArgumentParser(description=\
+        "MaltParser output processing pipeline for Russian.")
+    parser.add_argument("--input", help=\
+        "The input CoNLL file to be processed. Default is stdin.", default=None)
+    parser.add_argument("--output", help=\
+        "Output file. Default is stdout.", default=None)
     pa = parser.parse_args()
     lines = open(pa.input, "r") if pa.input else sys.stdin
     out = open(pa.output, "w") if pa.output else sys.stdout
     
-    sent_counter = 1
+    sent_count = 1
     for line in lines:
-        if line == "\n":
-            sent_counter += 1
+        line = line_splitter.split(line)
+        if len(line) > 1:
+            print line
+            term_id, form, lemma, cpostag, postag, feats, \
+                head, deprel, phead, pdeprel, _ = line
+            
+            
+            sent.append(lemma)
+            
+            
         else:
-            formatted = conll_mapper(line, sent_counter)
+            sent_count += 1
+            formatted = conll_mapper(line, sent_count)
             out.write(formatted.encode("utf-8"))
 
 
