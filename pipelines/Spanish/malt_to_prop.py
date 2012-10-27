82cad5f72ea8329c9e9a1311a3bb72003f257d74
Ross Israel <raisrael@indiana.edu>
2012-10-26 20:08:00 -0700

    making changes to malt_to_prop.py

--- pipelines/Spanish/malt_to_prop.py
+++ pipelines/Spanish/malt_to_prop.py
@@ -30,33 +30,43 @@ def to_prop(infile):
             props.append(prop)
         else:
             #print sentence
-            print "% <"+" ".join(sentence)+">"
+            print "% "+" ".join(sentence)
             #print ID(sentence number)
-            print "id(<"+str(sent_count)+">)."
+            print "id("+str(sent_count)+")."
             #loop over stored list of words and print props
             for i in range(len(props)):
-                if not propTags.match(props[0][2]):
+                if puncts.match(props[0][1]):
+                    #print "Punct",props[0][1]
+                    props.pop(0)
+                elif not propTags.match(props[0][2]):
                     propID = "["+str(sent_count)+str(props[0][0])+"]"
                     sys.stdout.write(propID+":"+props[0][1])
                     props.pop(0)
+                    if len(props) > 0:
+                        sys.stdout.write(" & ")                    
                 elif propTags.match(props[0][2]):
-                    simple_predicate,eCount,xCount = build_predicate(props[0][2],eCount,xCount)
-                    predicate = "("+",".join(simple_predicate)+")"
+                    predicate,eCount,xCount = build_predicate(props[0][2],eCount,xCount)
+                    #predicate = "("+",".join(simple_predicate)+")"
                     propID = "["+str(sent_count)+str(props[0][0])+"]"
-                    sys.stdout.write(propID+":"+props[0][1]+"-"+props[0][2]+predicate)
+                    sys.stdout.write(propID+":"+props[0][1]+"-"+predicate)
                     props.pop(0)
+                    if len(props) > 0:
+                        sys.stdout.write(" & ")                    
                 #place ampersand between words as long as the list of words has another entry
-                if len(props) > 0:
-                    sys.stdout.write(" & ")
+                #                if len(props) > 0:
+                #    sys.stdout.write(" & ")
             print ""
             sent_count += 1
             sentence = []
             props = []
+            eCount = 1
+            xCount = 1
             #print sent_count
 
 def build_predicate(pos,eCount,xCount):
     pred = []
     if verbTag.match(pos):
+        tag = "vb"
         pred.append("e"+str(eCount))
         eCount+=1
         pred.append("x"+str(xCount))
@@ -64,34 +74,43 @@ def build_predicate(pos,eCount,xCount):
         pred.append("x"+str(xCount))
         xCount+=1
         pred.append("x"+str(xCount))
-        xCount+=1        
-        return pred,eCount,xCount
+        xCount+=1
+        predicate=tag+"("+",".join(pred)+")"
+        return predicate,eCount,xCount
     if nounTag.match(pos):
+        tag="nn"
         pred.append("e"+str(eCount))
         eCount+=1
         pred.append("x"+str(xCount))
         xCount+=1
-        return pred,eCount,xCount
+        predicate=tag+"("+",".join(pred)+")"
+        return predicate,eCount,xCount
     if adjectiveTag.match(pos):
+        tag="adj"
         pred.append("e"+str(eCount))
         eCount+=1
         pred.append("x"+str(xCount))
         xCount+=1        
-        return pred,eCount,xCount
+        predicate=tag+"("+",".join(pred)+")"
+        return predicate,eCount,xCount
     if adverbTag.match(pos):
+        tag="rb"
         pred.append("e"+str(eCount))
         eCount+=1
         pred.append("x"+str(xCount))
         xCount+=1        
-        return pred,eCount,xCount
+        predicate=tag+"("+",".join(pred)+")"
+        return predicate,eCount,xCount
     if prepositionTag.match(pos):
+        tag="in"
         pred.append("e"+str(eCount))
         eCount+=1
         pred.append("x"+str(xCount))
         xCount+=1
         pred.append("x"+str(xCount))
         xCount+=1        
-        return pred,eCount,xCount
+        predicate=tag+"("+",".join(pred)+")"
+        return predicate,eCount,xCount
 
 def add_args(args,arg,count):
     args.append
@@ -103,6 +122,8 @@ if options.tagset == "ancora":
     adverbTag = re.compile("r")
     prepositionTag = re.compile("s")
     propTags = re.compile("n|v|a|r|s")
+
+puncts = re.compile("[\.,\?\!{}()\[\]:;¿¡]")
 
 def main():
     if options.input_FileName:
