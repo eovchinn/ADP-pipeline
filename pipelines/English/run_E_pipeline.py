98c6badcc4862225d536e2b6633b224f408e07d6
Ekaterina Ovchinnikova <ovchinnikova@ztm63.(none)>
2012-11-01 00:56:26 +0100

    How to run English pipeline.

---
+++ pipelines/English/run_E_pipeline.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import NLPipeline_coref_English_metaphor
+from NLPipeline_coref_English_metaphor import *
+
+if __name__ == "__main__":
+	print English_ADP("<META>'5'\n\n Every student reads.\n\n<META>'3'\n\n This dog runs. The animal is funny.")
