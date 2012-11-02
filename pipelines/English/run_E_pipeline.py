2b6f1a67d71153cbad7b98897cd22b3443389812
Ekaterina Ovchinnikova <ovchinnikova@ztm63.(none)>
2012-11-02 17:53:16 +0100

    English pipeline updated

--- pipelines/English/run_E_pipeline.py
+++ pipelines/English/run_E_pipeline.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
-import json
-
 import NLPipeline_coref_English_metaphor
 from NLPipeline_coref_English_metaphor import *
 
 if __name__ == "__main__":
-	output_struc = English_ADP("<META>'5'\n\n Every student reads.\n\n<META>'3'\n\n This dog runs. The animal is funny.")
-	print json.dumps(output_struc) 
+	print English_ADP("<META>'5'\n\n Every student reads.\n\n<META>'3'\n\n This dog runs. The animal is funny.") 
