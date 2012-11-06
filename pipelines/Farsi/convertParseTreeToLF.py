a0aababef30e07972ac22536a2128d5cb6a50c5c
Niloofar Montazeri <niloofar@Niloofar.local>
2012-11-06 00:05:46 -0800

    handling conjunctions added

--- pipelines/Farsi/convertParseTreeToLF.py
+++ pipelines/Farsi/convertParseTreeToLF.py
@@ -6,8 +6,7 @@ outputFile=codecs.open(sys.argv[2], encoding='utf-8',mode="w")
 translitDictFile=codecs.open(sys.argv[3], encoding='utf-8')
 
 #each rule is in the form "relationName":(headArgIndex,dependentArgIndex)
-rules={"SBJ":(1,1),"OBJ":(2,1),"NPOSTMOD":(1,1)}
-
+rules={"SBJ":(1,1),"OBJ":(2,1),"VCL":(2,0),"PRD":(0,0),"NPOSTMOD":(1,1)}
 
 #this is a global dict for easy handling of different POS schema
 POSStopList=["PUNC"]
@@ -17,7 +16,13 @@ argDict={"N":2,"V":4,"ADJ":2,"ADV":2,"PREP":3,"PR":2,"":1}
 transLitDict={}
 #tempFile=codecs.open("%s.tmp.txt"%sys.argv[3], encoding='utf-8',mode='w')
 
-
+def findProp(props,propId):
+    for prop in props:
+        (id,word,lemma,POS,args)=prop
+        if id==propId:
+            return prop
+    return None
+        
 def loadtranslitDict(translitDictFile):
     lines=translitDictFile.readlines()
     for line in lines:
@@ -53,6 +58,9 @@ def propToString(sentenceId,prop):
     (id,word,lemma,POS,args)=prop
     postfix=""
     if POS in postFixDict: postfix=postFixDict[POS]
+    print sentenceId
+    if id==22:
+        niloo=1
     token="[%s]:%s%s(%s)"%(sentenceId*1000+id,getTranslit(lemma),postfix,",".join(args))
     return token
     
@@ -95,9 +103,9 @@ def createPropDict(props):
         propDict[id]=prop
     return propDict
 
-def convertSetsToLists(EqualArgMap):
+def convertSetsToLists(equalArgSets):
     returnList=[]
-    for equalSet in EqualArgMap:
+    for equalSet in equalArgSets:
         returnList+=[sorted(list(equalSet))]
     return returnList
         
@@ -108,14 +116,10 @@ def getRepresentativeArgName(equalList):
         if re.match("e.*",argName):
             return argName
     return equalList[0]
-
-def resolveArgs(LF):
-    (sentenceId,props,rels)=LF
-    propDict=createPropDict(props)
-    equalArgMap=[]
+def getEqualArgSets(propDict,rels):
+    equalArgSets=[]
     for rel in rels:
-        (relName,dependent,head)=rel
-        
+        (relName,dependent,head)=rel    
         if relName not in rules:
             continue
               
@@ -131,33 +135,179 @@ def resolveArgs(LF):
         dependentArgName=dependentArgs[dependentArgIndex]
         
         foundSet=None
-        for equalitySet in equalArgMap:
+        for equalitySet in equalArgSets:
             if headArgName in equalitySet or dependentArgName in equalitySet:
                 foundSet=equalitySet
                 break
         if foundSet==None:
             foundSet=set()
-            equalArgMap+=[foundSet]
+            equalArgSets+=[foundSet]
         foundSet.add(headArgName)
         foundSet.add(dependentArgName)
     
-    equalArgMap=convertSetsToLists(equalArgMap)
+    equalArgSets=convertSetsToLists(equalArgSets)
+    return equalArgSets
+
+def getDependentId(rels,headId,relationName):
+    for rel in rels:
+        (relName1,depId1,headId1)=rel
+        if relationName==relName1 and headId==headId1:
+            return depId1
+    return None
+        
+    
+def getNounConjArgSets(props,rels):
+    nounConjArgSets=[]
+    tempList=[]
+    for rel in rels:
+        (relName,dependentId,headId)=rel
+        if relName== "NCONJ":
+            conj1Prop=findProp(props,headId)
+            conj2Id=getDependentId(rels,dependentId,"POSDEP")
+            if conj2Id==None:
+                continue
+            conj2Prop=findProp(props,conj2Id)
+            (conj1Id,conj1Word,conj1Lemma,conj1POS,conj1Args)=conj1Prop
+            (conj2Id,conj2Word,conj2Lemma,conj2POS,conj2Args)=conj2Prop
+            conj1Arg=conj1Args[1]
+            conj2Arg=conj2Args[1]
+            tempList+=[(conj1Arg,conj2Arg)]
+        
+    for (arg1,arg2) in tempList:
+        foundSet=None
+        for conjSet in nounConjArgSets:
+            if arg1 in conjSet or arg2 in conjSet:
+                foundSet=conjSet
+                break
+        if foundSet==None:
+            foundSet=set()
+            nounConjArgSets+=[foundSet]
+        foundSet.add(arg1)
+        foundSet.add(arg2)
+    
+    return nounConjArgSets
             
+def replaceEqualArgs(props,equalArgSets):
     for prop in props:
         (Id,Word,Lemma,POS,Args)=prop
         for i in range(0,len(Args)):
-            for equalSet in equalArgMap:
+            for equalSet in equalArgSets:
                 if Args[i] in equalSet:
                     Args[i]=getRepresentativeArgName(equalSet)
+    return props
+
+def getMaxPropId(props):
+    maxId=-1
+    for prop in props:
+        (Id,Word,Lemma,POS,Args)=prop
+        if Id>maxId:
+            maxId=Id
+    return maxId
         
-    return (sentenceId,props,rels)        
-        
-                
-            
-        
+def addModifiers(props,rels,originalProp,newProp):
+    global eventualityArgCounter
+    global propIdCounter
+    propIdCounter+=1
     
-     
+    (originalPropId,originalPropWord,originalPropLemma,originalPropPOS,originalPropArgs)=originalProp
+    (newPropId,newPropWord,newPropLemma,newPropPOS,newPropArgs)=newProp
+    
+    originalPropArg0=originalPropArgs[0]
+    newPropArgs0=newPropArgs[0]
+    
+    #we duplicate any proposition that has originalProp's 0th argument in it.
+    newModifierProps=[]
+#    verbModifierRels=["NVE","ENC","VPP","ADV","AJUCL"]
+    for prop in props:
+        (propId,word,lemma,POS,args)=prop    
+        if lemma=="NVE":
+            niloo=1    
+        for i in range(1,len(args)):
+            if args[i]==originalPropArg0: # we found a prop that has originalPropArg0 as one of its arguments -> duplicate this prop and replace the argument with new argument
+                newDependentPropArgs=list(args)
+                newDependentPropArgs[i]= newPropArgs0
+                newDependentPropArgs[0]="e%s"%(eventualityArgCounter+1)
+                eventualityArgCounter+=1
+                newModifierProp=(propIdCounter+1,word,lemma,POS,newDependentPropArgs)
+                propIdCounter+=1
+                newModifierProps+=[newModifierProp]
+    return newModifierProps
+            
+def createNewPropsForNounConjs(props,rels,nounConjArgSets):
+    global eventualityArgCounter
+    eventualityArgCounter+=1
+    newProps=[]
+    newModifiers=[]
+    global propIdCounter
+    propIdCounter+=1
+    for originalProp in props:
+        print originalProp
+        (originalPropId,Word,Lemma,POS,Args)=originalProp
+        if POS!=POSDict["V"]:
+            continue
+        propArgSet=set(Args)
+        
+        #if any of the arguments in nounConjArgSet appears in the proposition, then we need to duplicate that proposition for any of the other elements in nounConjArgSet
+        conjSets=[] # a collection of sets, each set corresponds to an argument of prop 
+        for i in range(1,4): # we assume max number of arguments is 4, skip the 0th argument which is the eventuality arg
+            if i>=len(Args):
+                conjSets+=[set(["dummy"])]
+                continue
+            conjSet=set([Args[i]])
+            
+            for nounConjArgSet in nounConjArgSets:
+                if nounConjArgSet.intersection(conjSet): #if these two sets have an overlap, add all elements from the nounConjArgSet to conjSets[i]
+                    conjSet=conjSet.union(nounConjArgSet)
+            conjSets+=[conjSet]
+        
+        for arg1 in conjSets[0]:    #remember that we don't have a conj set for the 0th argument. So the first conjSet (conjSet[0]) corresponds to the 1st arg
+            for arg2 in conjSets[1]:
+                for arg3 in conjSets[2]:
+                    newArgs=["e%s"%eventualityArgCounter]
+                    if arg1!="dummy":
+                        newArgs+=[arg1]
+                    if arg2!="dummy":
+                        newArgs+=[arg2]
+                    if arg3!="dummy":
+                        newArgs+=[arg3]
+                    
+                    if newArgs[1:]==Args[1:]: # if same argument, don't add anything
+                        continue
+                    newPropId=propIdCounter+1
+                    newProp=(newPropId,Word,Lemma,POS,newArgs)
+                    newModifiers+=addModifiers(props,rels,originalProp,newProp)
+                    newProps+=[newProp]
+                    eventualityArgCounter+=1
+                    propIdCounter+=1
+    return props+newProps+newModifiers
+
+def createNewPropsForLightVerbs(props,rels):
+    global eventualityArgCounter
+    global propIdCounter
+    propIdCounter+=1
+    newProps=[]
+    for rel in rels:
+        (relName,dependentId,headId)=rel
+        if relName in ["NVE","ENC"]:
+            eventualityArgCounter+=1
+            
+            dependentProp=findProp(props,dependentId)
+            (dependentPropId,dependentWord,dependentLemma,dependentPOS,dependentArgs)=dependentProp
+            
+            headProp=findProp(props,headId)
+            (headId,headWord,headLemma,headPOS,headArgs)=headProp
+            
+            newProp=(propIdCounter,"",relName,"",["e%s"%eventualityArgCounter,dependentArgs[0],headArgs[0]])
+            newProps+=[newProp]
+            propIdCounter+=1
+            
+    return props+newProps
+                
+
+
+        
 def createLF(tokens,sentenceId):
+    global propIdCounter
     props=[]
     rels=[]
     for token in tokens:
@@ -167,6 +317,7 @@ def createLF(tokens,sentenceId):
         props+=[(id,word,lemma,POS,getArgs(POS))]
         rels+=[(relName,id,dep)]
     LF=(sentenceId,props,rels)
+    propIdCounter=getMaxPropId(props)
     LF2=resolveArgs(LF)
     return LF2
 
@@ -182,10 +333,25 @@ def lfToString(lf):
     returnString= "%s\nid(%s).\n%s\n"%(" ".join(words),str(sentenceId),lfLine)
     returnString="% "+returnString
     return returnString
-        
+
+def resolveArgs(LF):
+    (sentenceId,props,rels)=LF
+    #add new props for light verbs (add preds like NVE,ENC)
+    props=createNewPropsForLightVerbs(props,rels)
+    
+    propDict=createPropDict(props)
+    equalArgSets=getEqualArgSets(propDict,rels)    
+    props=replaceEqualArgs(props,equalArgSets)
+    
+    nounConjArgSets=getNounConjArgSets(props,rels)
+    props=createNewPropsForNounConjs(props,rels,nounConjArgSets)
+           
+    return (sentenceId,props,rels)               
+
 unknownargCounter=0
 entityArgCounter=0
 eventualityArgCounter=0
+propIdCounter=0
 
 loadtranslitDict(translitDictFile)
 
