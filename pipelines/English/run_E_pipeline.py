b423d3e0ab71b6a1e034b0360eb5af3bf523b4ce
Ekaterina Ovchinnikova <ovchinnikova@ztm63.(none)>
2012-11-08 20:20:26 +0100

    Parser to Henry

--- pipelines/English/run_E_pipeline.py
+++ pipelines/English/run_E_pipeline.py
@@ -4,5 +4,5 @@ import NLPipeline_coref_English_metaphor
 from NLPipeline_coref_English_metaphor import *
 
 if __name__ == "__main__":
-	metaphors = {'5': 'Every student reads.', '3': 'This dog runs. The animal is funny.'}
+	metaphors = {'5': 'Few nations have gone through what you have gone through, and few nations in recent history have made the progress you\u2019ve made.', '3': 'This dog runs. The animal is funny.'}
 	print English_ADP(metaphors) 
---
+++ pipelines/common/IntParser2Henry.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+import re
+import os
+import json
+
+TMP_DIR = os.environ['TMP_DIR']
+
+def main():
+
+	parser = argparse.ArgumentParser( description="Discource processing pipeline." )
+	parser.add_argument( "--input", help="The input file to be processed.", default=None )
+	parser.add_argument( "--output", help="The output file", default=None )
+	parser.add_argument( "--nonmerge", help="Add nonmerge constraints.", action="store_true", default=False )
+	parser.add_argument( "--textid", help="Text ids added.", action="store_true", default=False )
+	parser.add_argument( "--cost", help="Input observation cost.", type=int, default=1 )
+
+	pa = parser.parse_args()	
+	
+	lines = open(pa.input, "r") if pa.input else sys.stdin
+	ofile = open(pa.output, "w") if pa.output else sys.stdout
+	
+	#metafile = open(os.path.join(TMP_DIR,"meta"), "r")
+	#meta = json.loads(metafile.readline())
+	#metafile.close()
+
+	output_str = ''
+	id_prop_args_pattern = re.compile('(\[(\d+)\]:)?([^\[\(]+)(\((.+)\))?')
+	sent_id_pattern = re.compile('id\((.+)\)')
+	text_id_pattern = re.compile('% TEXTID\(([^\s\t]+)')
+	sent_id = ''
+	prop_id_counter = 0
+	text_id = ''
+	just_TEXT_ID = 0
+	for line in lines:
+		if line.startswith('%'): 
+			TIDmatchObj = text_id_pattern.match(line)
+			if TIDmatchObj:
+				if text_id!='': ofile.write('))\n')
+				text_id = TIDmatchObj.group(1)
+				ofile.write('(O (name ' + text_id + ') (^')
+				just_TEXT_ID = 1
+		elif line.startswith('id('): 
+			SIDmatchObj = sent_id_pattern.match(line)
+			if SIDmatchObj:
+				sent_id = SIDmatchObj.group(1)
+				prop_id_counter = 0
+				if not pa.textid: ofile.write('(O (name ' + sent_id + ') (^') 
+			else: print 'Strange sent id: ' + line
+			
+			if just_TEXT_ID == 1: just_TEXT_ID = 2
+			else: just_TEXT_ID = 0
+		elif line.strip() and just_TEXT_ID!=2:
+			props = line.split(' & ')
+			for prop in props:
+				matchObj = id_prop_args_pattern.match(prop)
+				if matchObj:
+					prop_id_counter+= 1
+
+					if matchObj.group(2): word_id = matchObj.group(2)
+					else: word_id = 'ID'+str(prop_id_counter)
+
+					prop_name = matchObj.group(3)
+
+					if matchObj.group(5): prop_args = matchObj.group(5)
+					else: prop_args = ''
+				
+					nm = ''
+					if prop_args!='': 
+						prop_args = ' '+prop_args.replace(',',' ')
+						nm = ' (!='+prop_args+')'
+
+					ofile.write(' ('+prop_name+prop_args+' :'+str(pa.cost)+':'+sent_id+'-'+str(prop_id_counter)+':['+word_id+'])')
+					ofile.write(nm)
+				#else: print 'Strange proposition: ' + prop + '\n'				
+
+			if not pa.textid: ofile.write('))\n')
+
+	if text_id!='': ofile.write('))\n')
+	ofile.close()
+					 
+if "__main__" == __name__: main()
