89314f08739364d6f2c6b7820369647203dd5e86
Ekaterina Ovchinnikova <ovchinnikova@ztm63.(none)>
2012-11-14 02:21:34 +0100

    Multiple word ids

--- pipelines/common/IntParser2Henry.py
+++ pipelines/common/IntParser2Henry.py
@@ -6,8 +6,28 @@ import sys
 import re
 import os
 import json
+from collections import defaultdict
+
+# global vars
+id2prop = defaultdict(list)
 
 TMP_DIR = os.environ['TMP_DIR']
+
+def add_id2prop(id_str,arg_str):
+	ids = id_str.split(',')
+	args = arg_str.split(',')
+	for id in ids:
+		id2prop[id].append(args[0])	
+
+def generate_sameID_nm():
+	nm = ''
+	for id in id2prop.keys():
+		if len(id2prop[id])>1:
+			nm += ' (!='
+			for arg in id2prop[id]:
+				nm += ' ' + arg 
+			nm += ')'
+	return nm
 
 def main():
 
@@ -43,11 +63,11 @@ def main():
 				text_id = TIDmatchObj.group(1)
 				ofile.write('(O (name ' + text_id + ') (^')
 				just_TEXT_ID = 1
+				prop_id_counter = 0
 		elif line.startswith('id('): 
 			SIDmatchObj = sent_id_pattern.match(line)
 			if SIDmatchObj:
 				sent_id = SIDmatchObj.group(1)
-				prop_id_counter = 0
 				if not pa.textid: ofile.write('(O (name ' + sent_id + ') (^') 
 			#else: print 'Strange sent id: ' + line
 			
@@ -76,13 +96,19 @@ def main():
 					nm = ''
 					if prop_args!='': 
 						prop_args = ' '+prop_args.replace(',',' ')
-						if pa.nonmerge: nm = ' (!='+prop_args+')'
+						if pa.nonmerge: 
+							nm = ' (!='+prop_args+')'
+							if matchObj.group(2): add_id2prop(matchObj.group(2),matchObj.group(5))
 
 					ofile.write(' ('+prop_name+prop_args+' :'+str(pa.cost)+':'+sent_id+'-'+str(prop_id_counter)+':['+word_id+'])')
 					if pa.nonmerge: ofile.write(nm)
 				#else: print 'Strange proposition: ' + prop + '\n'				
-
-			if not pa.textid: ofile.write('))\n')
+			
+			if pa.nonmerge: ofile.write(generate_sameID_nm())
+			id2prop.clear()
+			if not pa.textid: 
+				ofile.write('))\n')
+				prop_id_counter = 0
 
 	if text_id!='': ofile.write('))\n')
 	ofile.close()
