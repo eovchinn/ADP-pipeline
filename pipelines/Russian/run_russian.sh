2a1d8ae358c79de813dae4c0d07520381b55c939
Vladimir Zaytsev <vladimir@zvm.me>
2012-11-30 01:37:16 -0800

    updated running script, updated gold

--- pipelines/Russian/run_russian.sh
+++ pipelines/Russian/run_russian.sh
@@ -30,10 +30,13 @@ TREE_TAGGER_CMD=$METAPHOR_DIR/external-tools/tree-tagger-3.2/$PLATFORM/cmd
 TREE_TAGGER_LIB=$METAPHOR_DIR/external-tools/tree-tagger-3.2/$PLATFORM/lib
 TREE_TAGGER_OPT="-lemma -token -sgml -quiet"
 
+NLTK_TOKENIZER_DIR=$METAPHOR_DIR/external-tools/nltk_tokenizer
+
 MALT_DIR=$METAPHOR_DIR/external-tools/malt-1.5
 MALT_RU_DIR=$METAPHOR_DIR/external-tools/malt-ru
 
-TOKENIZER_BIN=$TREE_TAGGER_CMD/utf8-tokenize.perl
+TOKENIZER_BIN=$NLTK_TOKENIZER_DIR/nltk_tokenizer.py
+
 TAGGER_BIN=$TREE_TAGGER_BIN/tree-tagger
 TAGGER_PAR=$MALT_RU_DIR/russian.par
 LEMMATIZER_BIN=$MALT_RU_DIR/lemmatiser.pl
@@ -45,15 +48,12 @@ MALT_MODEL=rus-test.mco
 MALT_IFORMAT=$MALT_RU_DIR/make-malt.pl
 
 
-# IFILE=$RU_PIPELINE_DIR/examples/raw_text_input.txt
-# OFILE=$RU_PIPELINE_DIR/examples/malt_ru_output.txt
-
 CURRENT_DIR=`pwd`
 cd $MALT_RU_DIR
 
 if [[ $PLATFORM == "linux" ]]; then
   if [ -d "$2" ]; then
-    $TOKENIZER_BIN < "${1:-/dev/stdin}" | # < $IFILE
+    python $TOKENIZER_BIN < "${1:-/dev/stdin}" |
     tee $2/tokenizer_o.txt |
     $TAGGER_BIN $TREE_TAGGER_OPT $TAGGER_PAR |
     tee $2/tagger_o.txt |
@@ -62,18 +62,18 @@ if [[ $PLATFORM == "linux" ]]; then
     $MALT_IFORMAT | 
     java -Xmx16g -jar $MALT_BIN -c $MALT_MODEL -m parse -v off |
     tee $2/malt_o.txt |
-    python $RU_PIPELINE_DIR/malt_ru.py > /dev/stdout # > $IFILE
+    python $RU_PIPELINE_DIR/malt_ru.py > /dev/stdout
   else
-    $TOKENIZER_BIN < "${1:-/dev/stdin}" | # < $IFILE
+    python $TOKENIZER_BIN < "${1:-/dev/stdin}" |
     $TAGGER_BIN $TREE_TAGGER_OPT $TAGGER_PAR |
     $LEMMATIZER_BIN -l $MALT_RU_DIR/msd-ru-lemma.lex.gz -p $MALT_RU_DIR/wform2011.ptn1 -c $MALT_RU_DIR/cstlemma |
     $MALT_IFORMAT | 
     java -Xmx16g -jar $MALT_BIN -c $MALT_MODEL -m parse -v off |
-    python $RU_PIPELINE_DIR/malt_ru.py > "${2:-/dev/stdout}" # > $IFILE
+    python $RU_PIPELINE_DIR/malt_ru.py > "${2:-/dev/stdout}"
   fi
 elif [[ $PLATFORM == "darwin" ]]; then
   if [ -d "$2" ]; then
-      $TOKENIZER_BIN < "${1:-/dev/stdin}" |
+      python $TOKENIZER_BIN < "${1:-/dev/stdin}" |
       tee $2/tokenizer_o.txt |
       $TAGGER_BIN $TREE_TAGGER_OPT $TAGGER_PAR |
       tee $2/tagger_o.txt |
@@ -82,7 +82,7 @@ elif [[ $PLATFORM == "darwin" ]]; then
       tee $2/malt_o.txt |
       python $RU_PIPELINE_DIR/malt_ru.py > /dev/stdout
   else
-      $TOKENIZER_BIN < "${1:-/dev/stdin}" |
+      python $TOKENIZER_BIN < "${1:-/dev/stdin}" |
       $TAGGER_BIN $TREE_TAGGER_OPT $TAGGER_PAR |
       $MALT_IFORMAT | 
       java -Xmx16g -jar $MALT_BIN -c $MALT_MODEL -m parse -v off |
