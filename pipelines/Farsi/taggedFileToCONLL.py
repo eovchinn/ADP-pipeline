b49f57b81bbacc4a9f64d807891ac3b70cdb07f9
Niloofar Montazeri <niloofar@Niloofar.local>
2012-12-06 16:21:40 -0800

    new updates

--- pipelines/Farsi/taggedFileToCONLL.py
+++ pipelines/Farsi/taggedFileToCONLL.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-
+# -*- coding: utf-8 -*-
 import sys
 import codecs
 import os
@@ -13,24 +13,42 @@ import os
 inputFile=codecs.open(sys.argv[1],encoding='utf-8') if len(sys.argv)>1 else codecs.getreader("utf-8")(sys.stdin)
 outputFile=codecs.open(sys.argv[2],encoding='utf-8',mode="w") if len(sys.argv)>2 else codecs.getreader("utf-8")(sys.stdout)
 
-
 metaphorDir=os.environ['METAPHOR_DIR']
 lemmaFile=codecs.open("%s/pipelines/Farsi/lemmatizationDict.txt"%metaphorDir, encoding='utf-8')
 
+pronounList=[u'\u0627\u0634',u'\u062a\u0627\u0646',u'\u0627\u0645',u'\u062a',u'\u0634',u'\u0634\u0627\u0646',u'\u0645\u0627\u0646',u'\u0645',u'\u0627\u062a']
+
+niloo=1
 
 def loadLemmaDict(lemmaFile):
     lemmaDict={}
     line=lemmaFile.readline()
+    lineNumber=1
     while line!="":  
         if line.strip()=="":
-            line=lemmaFile.readline() 
+            line=lemmaFile.readline()
+            lineNumber+=1 
             continue
-        (word,POS,lemma)=line.strip().split("\t")
+        (word,POS,lemma)=line.replace(u'\u200e',"").strip().split("\t")
         lemmaDict [(word,POS)]=lemma
         line=lemmaFile.readline() 
+        lineNumber+=1
+    
+    addLemmaForWordsWithpossesivePostFix(lemmaDict)
     return lemmaDict
+def addLemmaForWordsWithpossesivePostFix(lemmaDict):
+    #maybe our combination is not in lemmatization dict, try decomposing the word
+    #if the word ends with a pronoun, then replace the pronoun with "" and check whether the new word is in out lemmatization dict. If so, return the lemma of that new word
+    newItems={}
+    for (lemma,POS)in lemmaDict:
+        for pronoun in pronounList:
+            newItems[("%s%s"%(lemma,pronoun.replace(u'\u200e',"")),POS)]=lemma
+    for key in newItems:
+        lemmaDict[key]=newItems[key]
+                
 def getLemma(word,POS,lemmaDict):
-    if (word,POS) not in lemmaDict: return word
+    if (word,POS) not in lemmaDict: 
+        return word
     return lemmaDict[(word,POS)]
 
 lemmaDict=loadLemmaDict(lemmaFile)
@@ -52,9 +70,11 @@ while line!="":
     for i in range(0,len(words)):
         lemma=getLemma(words[i],POSs[i],lemmaDict)
         items=(str(i+1),words[i],lemma,POSs[i],POSs[i],"_","_","_","_","_")
+        #outputFile.write(("%s\n"%"\t".join(items)).encode('utf-8'))
         outputFile.write(("%s\n"%"\t".join(items)).encode('utf-8'))
+
     
-    outputFile.write(("\n").encode('utf-8'))
+    outputFile.write(("\n"))
     line=inputFile.readline()
     
 inputFile.close()
