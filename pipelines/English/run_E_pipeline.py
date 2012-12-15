5de1c0f1412644a816fc93198b0c8dc51ae6981e
Metaphor Project <metaphor@whale.(none)>
2012-12-14 16:13:17 -0800

    Remove old files

--- pipelines/English/run_E_pipeline.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import NLPipeline_coref_English_metaphor
-from NLPipeline_coref_English_metaphor import *
-
-if __name__ == "__main__":
-	metaphors = {'5': 'Few nations have gone through what you have gone through, and few nations in recent history have made the progress you\u2019ve made.', '3': 'This dog runs. The animal is funny.'}
-	print English_ADP(metaphors) 
