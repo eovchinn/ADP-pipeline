eb1c710acdd2e600b689b30bcee86b5d8983acbf
Maria Muslea <mariam@isi.edu>
2013-01-16 14:58:30 -0800

    encoding

--- external-tools/YAGO-tools/get_categories.py
+++ external-tools/YAGO-tools/get_categories.py
@@ -6,14 +6,13 @@ import psycopg2.extras
 import sys
 import codecs
 
-CONN_STRING =  "host='colo-vm19.isi.edu' dbname='yago' user='yago' password='yago'"
+CONN_STRING =  "host='localhost' dbname='yago' user='yago' password='yago'"
 con = None
 
 def main():
 
-  query="select distinct object from yagofacts where subject like '%@@@word@@@%'and predicate like 'rdf:type'"
+  query="select distinct subject, object from yagofacts where subject like '%@@@word@@@%'and predicate like 'rdf:type'"
 
-  sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
   #get inputs
   args=sys.argv
   if len(args)!=3:
@@ -32,11 +31,13 @@ def main():
 
   try:
     con = psycopg2.connect(CONN_STRING) 
+    con.set_client_encoding('UTF8')
     cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(query)
     #get result
     rows = cur.fetchall()
     for row in rows:
+    	#print row['subject'],row['object']
     	print row['object']
 
   except psycopg2.DatabaseError, e:
