d702df9b470763eb4a722dcacce3efd828426706
Maria Muslea <mariam@isi.edu>
2013-01-18 12:09:40 -0800

    lang in query

--- external-tools/YAGO-tools/get_categories.py
+++ external-tools/YAGO-tools/get_categories.py
@@ -11,28 +11,59 @@ con = None
 
 def main():
 
-  query_eng="select distinct object as category from yagofacts where subject like '%@@@word@@@%'and predicate like 'rdf:type'"
-  query_multi="select distinct yf1.object as category from yagofacts yf1, yagofacts yf2 where yf2.predicate like 'rdfs:label' and yf2.object like '%@@@word@@@%' and yf2.subject like yf1.subject and yf1.predicate like 'rdf:type'"
+  query_eng="select distinct subject as subject, object as category from yagofacts where subject like '@@@word@@@'and predicate like 'rdf:type'"
+  query_multi="select distinct yf2.object as subject, yf1.object as category from yagofacts yf1, yagofacts yf2 where yf2.predicate like 'rdfs:label' and yf2.object like '@@@word@@@' and yf2.subject like yf1.subject and yf1.predicate like 'rdf:type'"
 
   #get inputs
   args=sys.argv
-  if len(args)!=3:
-	print "Usage:" , args[0],  "\"word\" lang<EN|RU|FA|ES>"
-	print "Example:" , args[0],  "\"Barack Obama\" EN"
+  if len(args)<3:
+	print "Usage:" , args[0],  "\"word\" lang<EN|RU|FA|ES> <-s(optional)>"
+	print "Example:" , args[0],  "\"Barack Obama\" EN (exact match)"
+	print "Example:" , args[0],  "\"Barack Obama\" EN -s (substring match)"
 	sys.exit(0)
+
+  # default is exact match
+  substring=False
+  if len(args)==4:
+    #substring match
+    if args[3]=='-s':
+      substring=True
 
   inword=args[1]
   lang=args[2]
 
-  query = query_multi
+  #prepare language
+  qlang=None
+  if lang=='RU':
+    qlang='@rus'
+  elif lang=='ES':
+    qlang='@spa'
+  elif lang=='FA':
+    qlang='@fas'
+
+  #prepare search word
   if lang=='EN':
     query=query_eng
+    #replace spaces with _ only for EN
+    inword = inword.replace(' ','\_')
+    if substring:
+      inword = '%'+inword+'%'
+    else:
+      #exact match
+      inword = '<'+inword+'>'
+  else:
+    query = query_multi
+    if substring:
+      inword = '%'+inword+'%'+qlang
+    else:
+      #exact match
+      inword = '"'+inword+'"'+qlang
 
-  #replace spaces with _
-  inword = inword.replace(' ','_')
   #build query
   query = query.replace('@@@word@@@',inword)
   print "Query:",query
+
+
 
   try:
     con = psycopg2.connect(CONN_STRING) 
@@ -42,7 +73,7 @@ def main():
     #get result
     rows = cur.fetchall()
     for row in rows:
-    	print row['category']
+    	print row['subject'],row['category']
 
   except psycopg2.DatabaseError, e:
     print 'Error %s' % e    
