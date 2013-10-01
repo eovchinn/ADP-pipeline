fc9db4b3349c8469b2d5d258e79406e42e37f40b
Katya <katya@whale.(none)>
2013-10-01 11:39:57 +0200

    new CM output format for LCC

--- pipelines/common/extract_CMs_from_hypotheses.py
+++ pipelines/common/extract_CMs_from_hypotheses.py
@@ -5,41 +5,12 @@ import re
 from collections import defaultdict
 import json
 
-def removeDuplicates(labeledProps,Equalities):
-	no_duplicates_labeledProps = []
-	for (propName,args) in labeledProps:
-		found = False
-		for (propName2,args2) in no_duplicates_labeledProps:		
-			if propName2 == propName and len(args)==len(args2):
-				argsEqual = True
-				for i in range(len(args)):					
-					if args[i] == args2[i]: pass
-					elif Equalities.has_key(args[i]) and Equalities[args[i]].has_key(args2[i]): pass
-					elif Equalities.has_key(args2[i]) and Equalities[args2[i]].has_key(args[i]): pass
-					else:
-						argsEqual = False
-						break
-				if argsEqual:
-					found = True
-					break
-		if not found:
-			no_duplicates_labeledProps.append((propName,args))
-	#print json.dumps(no_duplicates_labeledProps, ensure_ascii=False)
-	return no_duplicates_labeledProps
+def wordStr2print(Args,WordProps,Equalities):
+	output_str = ''
 
-def wordStr2print(labeledProps,WordProps,Equalities):
-	domain2words = dict()
-	for (propName,args) in labeledProps:
-		words_str = ''
-		if len(args)>0:
-			words_str = findWords(args[0],WordProps,Equalities,False)
-		if len(words_str)>0:
-			if not domain2words.has_key(propName): domain2words[propName] = words_str
-			else: domain2words[propName] += ',' + words_str
-	
-	output_str = ''		
-	for propName in domain2words.keys():
-		output_str += ',' + propName + '[' + domain2words[propName] + ']'
+	for arg in Args:
+		newwords = findWords(arg,WordProps,Equalities,False)
+		if len(newwords)>0:	output_str += ',' + findWords(arg,WordProps,Equalities,False)
 	
 	return output_str[1:]
 
@@ -85,22 +56,88 @@ def findWords(ARG,WordProps,Equalities,isMapping):
 	if isMapping:	return ARG		
 	return ''
 
-def printPropNames(Props):
-	output_str = ''
-	been = dict()
+def createDStruc(superD,subD):
+	outputstrucs = defaultdict(dict)
 
-	for (propName,args) in Props:
-		if not been.has_key(propName):
-			output_str += ', ' + propName
-			been[propName] = 1
-	return output_str[2:]
+	for superd in superD:
+		for superArgs in superD[superd]:
+			if not outputstrucs.has_key(superd) or not outputstrucs[superd].has_key(superArgs[0]): outputstrucs[superd][superArgs[0]] = []
+			for subd in subD:
+				for subArgs in subD[subd]:
+					if len(subArgs)>1 and superArgs[0]==subArgs[1]:
+						outputstrucs[superd][superArgs[0]].append((subd,subArgs[0]))
+
+	#print json.dumps(outputstrucs, ensure_ascii=False)
+	return outputstrucs
+
+def collectVars(struc,superkey):
+	output = []
+
+	for arg in struc[superkey]:
+		if not arg.startswith('_'): output.append(arg)
+		for (subd,subarg) in struc[superkey][arg]:
+			if not subarg.startswith('_'): output.append(subarg)
+	return output
+
+def isLinkedbyParse(v1,v2,word_props,equalities,been):
+	if (v1,v2) in been: return False
+	been.append((v1,v2))
+	been.append((v2,v1))
+
+	if equalities.has_key(v1) and equalities[v1].has_key(v2): return True
+
+	for (propName,args) in word_props:
+		if v1 in args:
+			if v2 in args: 
+				return True
+			else:
+				if len(args)>2: 
+					i1 = args.index(v1)
+					if i1==0: 
+						if isLinkedbyParse(args[1],v2,word_props,equalities,been): return True
+						if isLinkedbyParse(args[2],v2,word_props,equalities,been): return True
+						if len(args)>3 and isLinkedbyParse(args[3],v2,word_props,equalities,been): return True
+					elif i1==1:
+						if isLinkedbyParse(args[0],v2,word_props,equalities,been): return True
+						if isLinkedbyParse(args[2],v2,word_props,equalities,been): return True
+						if len(args)>3 and isLinkedbyParse(args[3],v2,word_props,equalities,been): return True
+					elif i1==2:
+						if isLinkedbyParse(args[0],v2,word_props,equalities,been): return True
+						if isLinkedbyParse(args[1],v2,word_props,equalities,been): return True
+						if len(args)>3 and isLinkedbyParse(args[3],v2,word_props,equalities,been): return True
+					elif i1==3:
+						if isLinkedbyParse(args[0],v2,word_props,equalities,been): return True
+						if isLinkedbyParse(args[1],v2,word_props,equalities,been): return True
+						if isLinkedbyParse(args[2],v2,word_props,equalities,been): return True
+		elif v2 in args:
+			if len(args)>2: 
+				i2 = args.index(v2)
+				if i2==0: 
+					if isLinkedbyParse(args[1],v1,word_props,equalities,been): return True
+					if isLinkedbyParse(args[2],v1,word_props,equalities,been): return True
+					if len(args)>3 and isLinkedbyParse(args[3],v1,word_props,equalities,been): return True
+				elif i2==1:
+					if isLinkedbyParse(args[0],v1,word_props,equalities,been): return True
+					if isLinkedbyParse(args[2],v1,word_props,equalities,been): return True
+					if len(args)>3 and isLinkedbyParse(args[3],v1,word_props,equalities,been): return True
+				elif i2==2:
+					if isLinkedbyParse(args[0],v1,word_props,equalities,been): return True
+					if isLinkedbyParse(args[1],v1,word_props,equalities,been): return True
+					if len(args)>3 and isLinkedbyParse(args[3],v1,word_props,equalities,been): return True
+				elif i2==3:
+					if isLinkedbyParse(args[0],v1,word_props,equalities,been): return True
+					if isLinkedbyParse(args[1],v1,word_props,equalities,been): return True
+					if isLinkedbyParse(args[2],v1,word_props,equalities,been): return True
+
+	return False
 
 def extract_CM_mapping(id,inputString,DESCRIPTION):
 	#print inputString
-	targets = []	
-	subtargets = []
-	sources = []
-	subsources = []
+	targets = dict()	
+	subtargets = dict()
+	subsubtargets = dict()
+	sources = dict()
+	subsources = dict()
 	mappings = []
 	roles = []
 	word_props = []
@@ -118,13 +155,20 @@ def extract_CM_mapping(id,inputString,DESCRIPTION):
 			args = arg_str.split(',')
 
 			if prop_name.startswith('T#'):
-				targets.append((prop_name[2:],args))
+				if not targets.has_key(prop_name[2:]): targets[prop_name[2:]] = []
+				targets[prop_name[2:]].append(args)
 			elif prop_name.startswith('TS#'):
-				subtargets.append((prop_name[3:],args))
+				if not subtargets.has_key(prop_name[3:]): subtargets[prop_name[3:]] = []
+				subtargets[prop_name[3:]].append(args)
+			elif prop_name.startswith('TSS#'):
+				if not subsubtargets.has_key(prop_name[4:]): subsubtargets[prop_name[4:]] = []
+				subsubtargets[prop_name[4:]].append(args)
 			elif prop_name.startswith('S#'):
-				sources.append((prop_name[2:],args))
+				if not sources.has_key(prop_name[2:]): sources[prop_name[2:]] = []
+				sources[prop_name[2:]].append(args)
 			elif prop_name.startswith('SS#'):
-				subsources.append((prop_name[3:],args))
+				if not subsources.has_key(prop_name[3:]): subsources[prop_name[3:]] = []
+				subsources[prop_name[3:]].append(args)
 			elif prop_name.startswith('M#'):
 				mappings.append((prop_name[2:],args))
 			elif prop_name.startswith('R#'):
@@ -145,58 +189,115 @@ def extract_CM_mapping(id,inputString,DESCRIPTION):
 				if prop_name == 'equal':
 					equalities[args[1]][args[2]]=1
 					equalities[args[2]][args[1]]=1
-				word_props.append((prop_name,args))
+				else: word_props.append((prop_name,args))
 
-	#remove duplicates
-	#targets = removeDuplicates(targets,equalities)
-	#subtargets = removeDuplicates(subtargets,equalities)
-	#sources = removeDuplicates(sources,equalities)
-	#subsources = removeDuplicates(subsources,equalities)
-	mappings = removeDuplicates(mappings,equalities)
+	#print json.dumps(sources, ensure_ascii=False)
+
+	target_strucs = createDStruc(subtargets,subsubtargets)
+	source_strucs = createDStruc(sources,subsources)
 
 	output_struct_item = {}
 	output_struct_item["id"] = id
 	output_struct_item["isiDescription"] = DESCRIPTION
+	output_struct_item["targetConceptDomain"] = "ECONOMIC_INEQUALITY"
 
-	output_struct_item["targetConceptDomain"] = printPropNames(targets)
-	output_struct_item["targetConceptSubDomain"] = printPropNames(subtargets)
+	explanationAppendix = "\n%%BEGIN_CM_LIST\n"
 
-	output_struct_item["sourceFrame"] = printPropNames(sources)
-	output_struct_item["sourceConceptSubDomain"] = printPropNames(subsources)#
+	CMlinked = ''
+	CMunlinked = ''
 
-	output_struct_item["targetFrame"] = output_struct_item["targetConceptSubDomain"]
+	for targetS in target_strucs:
+		tV = collectVars(target_strucs,targetS)
+
+		Tstrings = []
+
+		for targ in target_strucs[targetS]: 
+			if len(target_strucs[targetS][targ])==0: 
+				Tstrings.append('ECONOMIC_INEQUALITY,' + targetS + ',' + targetS)
+			else: 		
+				for (tsubd,tsubarg) in target_strucs[targetS][targ]:
+					Tstrings.append('ECONOMIC_INEQUALITY,' + targetS + ',' + tsubd)
+
+		for sourceS in source_strucs:
+			sV = collectVars(source_strucs,sourceS)
+			linked = False
+			for tv in tV:
+				for sv in sV:
+					if isLinkedbyParse(tv,sv,word_props,equalities,[]):
+						linked = True
+						break
+				if linked: break
+
+			Sstrings = []
+			for sarg in source_strucs[sourceS]:
+				if len(source_strucs[sourceS][sarg])==0:
+					Sstrings.append(','+sourceS+',-')
+				else:
+					for (ssubd,ssubarg) in source_strucs[sourceS][sarg]:
+						Sstrings.append(','+sourceS+','+ssubd)
+
+			for ts in Tstrings:
+				for ss in Sstrings:
+					explanationAppendix += ts+ss
+					if linked: 
+						explanationAppendix += ',0.9\n'
+						CMlinked = ts+ss
+					else: 
+						explanationAppendix += ',0.3\n'
+						CMunlinked = ts+ss
+
+	explanationAppendix+="%%END_CM_LIST"
+
+	output_struct_item['isiAbductiveExplanation'] = inputString + explanationAppendix
+
+	bestCM = CMlinked
+	if len(bestCM)==0: bestCM = CMunlinked
+
+	if len(bestCM)==0:
+		output_struct_item["targetConceptDomain"] = ''
+		output_struct_item["targetFrame"] = ''
+		output_struct_item["sourceFrame"] = ''
+		output_struct_item["sourceConceptSubDomain"] = ''
+		output_struct_item['annotationMappings'] = []
+	else:
+		data = bestCM.split(',')
+		output_struct_item["targetConceptDomain"] = data[1]
+		output_struct_item["targetFrame"] = data[2]
+		output_struct_item["sourceFrame"] = data[3]
+		if data[4]=='-': output_struct_item["sourceConceptSubDomain"] = ''
+		else: output_struct_item["sourceConceptSubDomain"] = data[4]
+
+		targetArgs = dict()
+		if subtargets.has_key(data[1]):
+			for args in subtargets[data[1]]:
+				targetArgs[args[0]]=1
+		if subsubtargets.has_key(data[2]):
+			for args in subsubtargets[data[2]]:
+				targetArgs[args[0]]=1
+
+		sourceArgs = dict()
+		if sources.has_key(data[3]):
+			for args in sources[data[3]]:
+				sourceArgs[args[0]]=1
+		if subsources.has_key(data[4]):
+			for args in subsources[data[4]]:
+				sourceArgs[args[0]]=1
+
+		targetWords = wordStr2print(targetArgs,word_props,equalities)
+		sourceWords = wordStr2print(sourceArgs,word_props,equalities)
+
+		mapping_str = wordStr2print_Mapping(mappings,word_props,equalities)
+
+		annotationMappings_struc = dict()
+		annotationMappings_struc['explanation'] = mapping_str
+		annotationMappings_struc['target'] = targetWords
+		annotationMappings_struc['source'] = sourceWords
+		annotationMappings_struc['targetInLm'] = False
+		annotationMappings_struc['sourceInLm'] = False
+
+		output_struct_item['annotationMappings'] = [annotationMappings_struc]
 	
-	targetWords = wordStr2print(targets,word_props,equalities)
-	subtargetWords = wordStr2print(subtargets,word_props,equalities)
-	if len(targetWords)>0 and len(subtargetWords)>0: targetWords += ',' + subtargetWords
-	else: targetWords += subtargetWords
 
-	sourceWords = wordStr2print(sources,word_props,equalities)
-	subsourceWords = wordStr2print(subsources,word_props,equalities)
-	if len(sourceWords)>0 and len(subsourceWords)>0: sourceWords += ',' + subsourceWords
-	else: sourceWords += subsourceWords
+	# #print json.dumps(output_struct_item, ensure_ascii=False)
 
-	mapping_str = wordStr2print_Mapping(mappings,word_props,equalities)
-
-	output_struct_item["targetFrameElementSentence"] = targetWords
-	output_struct_item["sourceFrameElementSentence"] = sourceWords
-
-	annotationMappings_struc = dict()
-	annotationMappings_struc['explanation'] = mapping_str
-	annotationMappings_struc['target'] = targetWords
-	annotationMappings_struc['source'] = sourceWords
-	annotationMappings_struc['targetInLm'] = False
-	annotationMappings_struc['sourceInLm'] = False
-
-	output_struct_item['annotationMappings'] = [annotationMappings_struc]
-	output_struct_item['isiAbductiveExplanation'] = inputString
-
-	#if len(targets)>0 and len(sources)>0:
-	#	output_struct_item["isiMetaphorConfirmed"] = 'YES'
-	#	output_struct_item["isiTargetSourceMapping"] = wordStr2print_Mapping(mappings,word_props,equalities)
-	#else:
-	#	output_struct_item["isiMetaphorConfirmed"] = 'NO'
-	#	output_struct_item["isiTargetSourceMapping"] = ''
-
-	#print json.dumps(output_struct_item, ensure_ascii=False)
 	return output_struct_item
