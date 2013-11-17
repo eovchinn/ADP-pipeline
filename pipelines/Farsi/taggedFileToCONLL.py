3a942057d52517f42b1a2aa1df3a89fc32840f62
Niloofar Montazeri <niloofar@niloofar.mshome.net>
2013-11-16 16:25:43 -0800

    correcting taxation issue in the pipeline

--- pipelines/Farsi/taggedFileToCONLL.py
+++ pipelines/Farsi/taggedFileToCONLL.py
@@ -3,6 +3,7 @@
 import sys
 import codecs
 import os
+import re
 
 #inputFormat: Each sentence is in a line:
 #    word1_POS1 word2_POS2 ...
@@ -13,13 +14,18 @@ import os
 inputFile=codecs.open(sys.argv[1],encoding='utf-8') if len(sys.argv)>1 else codecs.getreader("utf-8")(sys.stdin)
 outputFile=codecs.open(sys.argv[2],encoding='utf-8',mode="w") if len(sys.argv)>2 else codecs.getreader("utf-8")(sys.stdout)
 
+#tmpFile=codecs.open("/temp/testdelme.txt",encoding='utf-8',mode="w")
+
 metaphorDir=os.environ['METAPHOR_DIR']
 lemmaFile=codecs.open("%s/pipelines/Farsi/lemmatizationDict.txt"%metaphorDir, encoding='utf-8')
-testLemmaFile=codecs.open("%s/pipelines/Farsi/testLemmatizationDict.txt"%metaphorDir,encoding='utf-8',mode="w")
+#testLemmaFile=codecs.open("%s/pipelines/Farsi/testLemmatizationDict.txt"%metaphorDir,encoding='utf-8',mode="w")
 
-pronounList=[u'\u0627\u0634',u'\u062a\u0627\u0646',u'\u0627\u0645',u'\u062a',u'\u0634',u'\u0634\u0627\u0646',u'\u0645\u0627\u0646',u'\u0645',u'\u0627\u062a']
+#pronounList=[u'\u0627\u0634',u'\u062a\u0627\u0646',u'\u0627\u0645',u'\u062a',u'\u0634',u'\u0634\u0627\u0646',u'\u0645\u0627\u0646',u'\u0645',u'\u0627\u062a']
 
-POSTFIXES = [
+# nondashPostFixes are used for identifying postfixes that are attached to the word without dash.
+nonDashPostFixes={u"ا":[u"یم",u"یت",u"یش",u"یمان",u"یتان",u"یشان",u"یی"],u"و":[u"یم",u"یت",u"یش",u"یمان",u"یتان",u"یشان",u"یی"],"":[u"م",u"ت",u"ش",u"مان",u"تان",u"شان",u"ها",u"های",u"هایی",u"هایم",u"هایت",u"هایمان",u"هایتان",u"هایشان",u"ی"]}
+adjPostFixes=[u"تر",u"ترین",u"تران",u"ترها",u"ترینها",u"ترهای",u"ترینهای",u"ترانی",u"ترینهایی",u"ترینی",u"ترانی",u"تری"]
+POSTFIXES = [ # used for "word-postfix"
         u"ام",
         u"ات",
         u"اش",
@@ -37,7 +43,7 @@ POSTFIXES = [
         u"ترین",
         u"هایم",
         u"های",
-        u"یهای",
+        u"هایی",
         u"هایمان",
         u"هایتان",
         u"هایت",
@@ -47,14 +53,24 @@ POSTFIXES = [
         u"ایم",
         u"اید",
         u"اند",
+        u"ترین",
+        u"تران",
+        u"ترها",
+        u"ترینها",
+        u"ترهای",
+        u"ترینهای",
+        u"ترانی",
+        u"ترینهایی",
+        u"ترینی",
+        u"ترانی",
+        u"تری"
 ]
 
+#POSTFIXES = [p.encode("utf-8") for p in POSTFIXES]
 
-#for pronoun in pronounList:
-#    print (pronoun,"PRO")
-niloo=1
 
 def loadLemmaDict(lemmaFile):
+    lemmaSet=set()
     lemmaDict={}
     line=lemmaFile.readline()
     lineNumber=1
@@ -65,26 +81,14 @@ def loadLemmaDict(lemmaFile):
             continue
 #        print lineNumber
         (word,POS,lemma)=line.replace(u'\u200e',"").strip().split("\t")
+        lemmaSet.add((POS,lemma))
         lemmaDict [(word,POS)]=lemma
-        testLemmaFile.write("%s\t%s\n"%(word,str((word,POS))))
+        #testLemmaFile.write("%s\t%s\n"%(word,str((word,POS))))
         line=lemmaFile.readline() 
-        lineNumber+=1
-    
-    addLemmaForWordsWithpossesivePostFix(lemmaDict)
-    return lemmaDict
-def addLemmaForWordsWithpossesivePostFix(lemmaDict):
-    #maybe our combination is not in lemmatization dict, try decomposing the word
-    #if the word ends with a pronoun, then replace the pronoun with "" and check whether the new word is in out lemmatization dict. If so, return the lemma of that new word
-    newItems={}
-    for (lemma,POS)in lemmaDict:
-        if lemma==u'\u0627\u0647\u062f\u0627\u0641':
-            niloo=1
-        for pronoun in pronounList:
-            newItems[("%s%s"%(lemma,pronoun.replace(u'\u200e',"")),POS)]=lemma
-    for key in newItems:
-        lemmaDict[key]=newItems[key]
-                
-def getLemma(word,POS,lemmaDict):
+        lineNumber+=1    
+    return (lemmaDict,lemmaSet)
+               
+def getLemma(word,POS,lemmaDict,lemmaSet):
     #first check whether the word matches word-POSTFIX , then return word
     dashIndex=-1
     for i in range(len(word)-1,-1,-1):
@@ -92,17 +96,40 @@ def getLemma(word,POS,lemmaDict):
             dashIndex=i
             break
     if dashIndex>-1 and word[dashIndex+1:] in POSTFIXES:
+        #tmpFile.write("%s\n"%word[0:dashIndex])
         return word[0:dashIndex]
     
     
     #otherwise, refer to lemmatization dict
-    if (word,POS) not in lemmaDict: 
-#        print "NILOO:%s\t%s"%(word,str((word,POS)))
-        return word
+    if (word,POS) in lemmaDict: 
+        return lemmaDict[(word,POS)]
     
-    return lemmaDict[(word,POS)]
+    #otherwise, there might be a postfix with no dash, such that the lemmatization dict does not contain it. 
+    if POS=="ADJ" or POS=="adj":
+        for postfix in adjPostFixes:
+            return re.sub("%s$"%postfix,"",word)
+    
+    if POS=="N" or POS=="n": # first we handle postfixes for which we require the word to have a special end char
+        for endChar in nonDashPostFixes:
+            if endChar=="": continue
+            for postFix in nonDashPostFixes[endChar]:
+                lemma=re.sub("%s$"%postFix,"",word)
+                if lemma[-1]!=endChar: continue
+                # now we have a potential lemma that ends with endChar and has a postfix that fits its endchar, check whether this lemma exists in our lemmatization dict
+                if (POS,lemma) in lemmaSet:
+                    return lemma
+    
+    if POS=="N" or POS=="n": #now we handle normal postfixes 
+        for postFix in nonDashPostFixes[""]:
+            lemma=re.sub("%s$"%postFix,"",word)
+            # now we have a potential lemma
+            if (POS,lemma) in lemmaSet:
+                return lemma
+    
+    # no lemma found, return the word itself
+    return word    
 
-lemmaDict=loadLemmaDict(lemmaFile)
+(lemmaDict,lemmaSet)=loadLemmaDict(lemmaFile)
 line=inputFile.readline()
 while line!="":
     words=[]
@@ -119,7 +146,7 @@ while line!="":
         words+=[word]
         POSs+=[POS]
     for i in range(0,len(words)):
-        lemma=getLemma(words[i],POSs[i],lemmaDict)
+        lemma=getLemma(words[i],POSs[i],lemmaDict,lemmaSet)
         items=(str(i+1),words[i],lemma,POSs[i],POSs[i],"_","_","_","_","_")
         outputFile.write(("%s\n"%"\t".join(items)).encode('utf-8'))
         #outputFile.write(("%s\n"%"\t".join(items)))
@@ -130,7 +157,7 @@ while line!="":
     
 inputFile.close()
 outputFile.close()
-testLemmaFile.close()
-    
+#testLemmaFile.close()
+#tmpFile.close()    
 
 
